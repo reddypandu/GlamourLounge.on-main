bf3556f637e24682bc12eadcf3c4895a93449c24
reddypandu <128275448+reddypandu@users.noreply.github.com>
2023-11-16 11:54:46 +0530

    new file:   .vscode/settings.json

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post("/send-email", (req, res) => {
+  const { name, email, message } = req.body;
+
+  // Set up Nodemailer transporter
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "pandureddypatterns@gmail.com", // replace with your Gmail address
+      pass: "Pandu@12", // replace with your Gmail password
+    },
+  });
+
+  // Set up email options
+  const mailOptions = {
+    from: "your@gmail.com",
+    to: "pandureddypatterns@gmail.com", // replace with the recipient's email address
+    subject: "New Form Submission",
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error.toString());
+    }
+    res.status(200).send("Email sent: " + info.response);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
